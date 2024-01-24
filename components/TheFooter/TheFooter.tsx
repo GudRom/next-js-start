15de366d033b1ccd6a42e1cf696f8caa52a0e227
Roman <gudozhnikoff.rom@yandex.ru>
2024-01-24 17:57:00 +0300

    base next app

---
+++ components/TheFooter/TheFooter.tsx
@@ -0,0 +1,9 @@
+
+
+const TheFooter = () => {
+    return <footer className="container" style={{flex: '0 0 auto', padding: '10px 0'}}>
+        &copy; Created by me. 2024
+    </footer>;
+};
+
+export default TheFooter;
---
+++ components/TheHeader/TheHeader.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+
+const TheHeader = () => {
+    return <header className="header">
+        <Link href='/'>Home</Link>
+        <Link href='/about'>About</Link>
+        <Link href='/blog'>Blog</Link>
+    </header>;
+};
+
+export default TheHeader;
